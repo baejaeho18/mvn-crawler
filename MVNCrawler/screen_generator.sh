f81d0f47e3dc6b3c69911bb81389f5f841948a65
baejaeho18 <human@handong.ac.kr>
2023-10-22 13:12:07 +0900

    complete 17

--- MVNCrawler/screen_generator.sh
+++ MVNCrawler/screen_generator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in {1..20}; do
-  screen -S bfs_maven$i -dm bash -c "python bfs_maven$i.py --q q_items$i.txt --f projectList$i.csv"
+  screen -S bfs$i -dm bash -c "python bfs_maven.py --q q_items$i.txt --f projectList$i.csv"
 done
