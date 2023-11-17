079c14bc66a74ae5aaf6880c92f73d9b0b30a5c4
baejaeho18 <human@handong.ac.kr>
2023-11-17 16:01:34 +0900

    regular reporting

--- MVNCrawler/maven/merger.py
+++ MVNCrawler/maven/merger.py
@@ -1,11 +1,11 @@
 content = ""
-for cnt in range(1,21):
+for cnt in range(13,21):
     with open(f"projectList{cnt}.csv", 'r') as input:
         content = input.read()
-    with open("projectList.csv", "a") as output:
+    with open("projectList13_20.csv", "a") as output:
         output.write(content)
 
-with open(f"projectList4.txt", 'r') as input:
-    content = input.read()
-with open("projectList.csv", "a") as output:
-    output.write(content)      
+# with open(f"projectList4.txt", 'r') as input:
+#    content = input.read()
+# with open("projectList.csv", "a") as output:
+#    output.write(content)      
